f910c2eeec3a52322b8eefdf1feb656a6cdcf595
taulant.disha <taulantdisha@Taulants-MacBook-Pro.local>
2022-10-08 22:23:38 +0300

    Added marker and sentry

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,33 @@
 import { Modal } from "@redq/reuse-modal";
+import { useEffect } from "react";
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
+import SentryRRWeb from '@sentry/rrweb';
+import markerSDK from '@marker.io/browser';
 
 export function reportWebVitals(metric) {
   console.log(metric);
 }
 
 export default function CustomApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    Sentry.init({
+      dsn: `https://a0fa52f3582041b4adeb1ac3fa64f91b@o1432500.ingest.sentry.io/${process.env.NEXT_PUBLIC_SENTRY}`,
+      integrations: [new BrowserTracing(), new SentryRRWeb({})],
+      tracesSampleRate: 1.0,
+    });
+    markerSDK.loadWidget({
+      destination: `${process.env.NEXT_PUBLIC_MARKER}`,
+      reporter: {
+        email: 'frontend@aut.id',
+        fullName: 'Dao Hacker',
+      },
+      ssr: {
+        renderDelay: 5000, // 0 - 15000 (ms)
+      },
+    });
+  }, [])
   return (
     <Modal>
       <Component {...pageProps} />
